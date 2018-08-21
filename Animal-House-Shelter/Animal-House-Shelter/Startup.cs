e161ed7a47c5c94ec17904164076fdbe4c1d7384
teqow <bartosz.skowronek96@gmail.com>
2018-08-21 21:37:51 +0200

    Initial Commit

---
+++ Animal-House-Shelter/Animal-House-Shelter/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Animal_House_Shelter
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseStatusCodePages();
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseAuthentication();
+
+            //Change
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Animal-House-Shelter
+
+## General Info:
+Animal-House-Shelter for dogs and cats. Using ASP.Net Core 2.1 MVC, EF, Bootstrap, Jquery.
+
+## TODO List:
+-Add Controllers/Models/Views/DB
+-Identity
+-Administration
+-Clear Front-end and great Back-end
+-Generating a Report about status shelter 
