0822a2d256c359b7e6904e2101ffbfc26913aeba
teqow <bartosz.skowronek96@gmail.com>
2018-08-22 17:34:43 +0200

    front + todo logic

---
+++ Animal-House-Shelter/Models/Cats.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace src.Models
+{
+    public class Cats
+    {
+        //need to think about property
+    }
+}
---
+++ Animal-House-Shelter/Models/Dogs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Animal_House_Shelter.Models
+{
+    public class Dogs
+    {
+        //need to think about property
+
+    }
+}
