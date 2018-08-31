31ba9d04a423d841f1f164692a4574fd300b4374
teqow <bartosz.skowronek96@gmail.com>
2018-08-31 19:22:50 +0200

    todo admin functions

--- Animal-House-Shelter/Controllers/AdminController.cs
+++ Animal-House-Shelter/Controllers/AdminController.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Animal_House_Shelter.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Animal_House_Shelter.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly ICatRepository _catRepository;
+        private readonly IDogRepository _dogRepository;
+
+        public AdminController(ICatRepository catRepository, IDogRepository dogRepository)
+        {
+            _catRepository = catRepository;
+            _dogRepository = dogRepository;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        //GET List of Dogs
+        public IActionResult DogsList()
+        {
+            return View(_dogRepository.Dogs);
+        }
+
+        //GET List of Cats
+        public IActionResult CatsList()
+        {
+            return View(_catRepository.Cats);
         }
     }
 }
